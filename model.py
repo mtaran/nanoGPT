d2a32b8f2e20a0b505a515992e56373c73bdc6ed
Maksym Taran <mtaran@github.com>
2024-03-20 23:10:40 -0700

    if n embed == n output bottleneck, skip the bottleneck

--- model.py
+++ model.py
@@ -193,7 +193,7 @@ class GPT(nn.Module):
 
         if targets is not None:
             # if we are given some desired targets also calculate the loss
-            logits = self.lm_head(self.out_bottleneck(x))
+            logits = self.lm_head(self.out_bottleneck(x)) if self.config.n_outb != self.config.n_embd else self.lm_head(x)
             loss = F.cross_entropy(
                 logits.view(-1, logits.size(-1)),
                 (targets % 8192 if self.space_embedding else targets).view(-1), ignore_index=-1,
